fc2b3cd034bcefdf41996b2d741833aab67bf436
Jenell-M-Hogg <jenellmhogg@live.com>
2015-12-02 14:21:54 -0500

    I hope I don't break it

---
+++ FridgeMilestone3/FridgeMilestone3/Global/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FridgeMilestone3.Global
+{
+    class Constants
+    {
+        public static int frameWidth = 315;  
+        public static int frameHeight = 384;
+    }
+}
