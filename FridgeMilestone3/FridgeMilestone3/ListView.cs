ed7a4a8c0c1f581453b36473ef88fe9ba8afd7af
Jenell-M-Hogg <jenellmhogg@live.com>
2015-12-04 20:47:09 -0500

    Update

--- FridgeMilestone3/FridgeMilestone3/ListView.cs
+++ FridgeMilestone3/FridgeMilestone3/ListView.cs
@@ -16,5 +16,15 @@ namespace FridgeMilestone3
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
